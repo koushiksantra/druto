c6ba5a343f0f3825e3d0f5ecd9a5716aeff8b8d6
Biswarup Adhikari <biswarupadhikari@gmail.com>
2014-04-01 21:50:40 +0530

    Get data fetch problem solved by removing ? sign from htaccess

---
+++ Modules/User/Controllers/UserController.php
@@ -0,0 +1,10 @@
+<?php
+namespace Modules\User\Controllers;
+class UserController
+{
+
+	function profile($username)
+	{
+		echo "User Profile Page $username";
+	}
+}
--- Modules/User/Route.php
+++ Modules/User/Route.php
@@ -8,3 +8,36 @@ Route::get('/akash/([a-z]+)/([0-9]+)',function($lastname,$age)
 {
 	echo "I am Akash $lastname My age is $age";
 });
+Route::get('/userprofile/([a-z_]+)','Modules\User\Controllers\UserController@profile');
+
+Route::get('/dipu/([a-z]+)',function($title)
+{
+		echo "Dipanjan $title";
+});
+
+Route::get('/sam',function()
+{
+	p($_REQUEST);
+	echo "Sam Via GET";
+});
+Route::post('/sam',function()
+{
+	p($_REQUEST);
+	echo "Sam Via POST";
+});
+Route::put('/sam',function()
+{
+	parse_str(file_get_contents("php://input"),$post_vars);
+	p($post_vars);
+	echo "Sam Via PUT";
+});
+Route::delete('/sam',function()
+{
+	p($_REQUEST);
+	echo "Sam Via DELETE";
+});
+Route::patch('/sam',function()
+{
+	p($_REQUEST);
+	echo "Sam Via PATCH";
+});
